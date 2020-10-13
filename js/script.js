b62a31c45c6a5bd17583642d7ab329181290e1c3
unrealium <72078475+unrealium@users.noreply.github.com>
2020-10-13 15:03:21 +0300

    axit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('.icon-burger').click(function(event){
+	$(this).toggleClass('active');
+	$('.navigation__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+(function($) {
+  $(function() {
+    $("nav.tab__items").on("click", "a:not(.active)", function() {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active")
+        .closest("div.tab")
+        .find("div.tab__content")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
+    });
+  });
+})(jQuery);
+
+document.addEventListener('gesturestart', function(e){
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
