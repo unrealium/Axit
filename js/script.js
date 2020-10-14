f12259c4041df0a10902a5f9ea048e43ecd69640
unrealium <72078475+unrealium@users.noreply.github.com>
2020-10-14 15:10:51 +0300

    v.1.2.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-$('.icon-burger').click(function(event){
-	$(this).toggleClass('active');
-	$('.navigation__body').toggleClass('active');
-	$('body').toggleClass('lock');
+$(document).ready(function(){
+  $('.icon-burger').click(function(event){
+  	$(this).toggleClass('active');
+  	$('.navigation__body').toggleClass('active');
+  	$('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function(){
+  $('.price__spoiler').click(function(event){
+    if($('.block7__body').hasClass('one')){
+      $('.price__spoiler').not($(this)).removeClass('active');
+      $('.price__body').not($(this).next()).slideUp(400);
+    }
+    $(this).toggleClass('active').next().slideToggle(400);
+  });
 });
 
 (function($) {
